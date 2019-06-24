3acbfddbbd556606fc359b02d4ecb4ea9d45e942
Jérôme <jerome.ceccato@gmail.com>
2019-06-24 20:59:09 +0200

    Base project

---
+++ Pkmn/Sources/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Pkmn
+//
+//  Created by Jerome Ceccato on 24/06/2019.
+//  Copyright © 2019 IATGOF. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Pkmn/Sources/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Pkmn
+//
+//  Created by Jerome Ceccato on 24/06/2019.
+//  Copyright © 2019 IATGOF. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
